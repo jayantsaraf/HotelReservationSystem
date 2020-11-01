b34ae8bfaa184ba8a663589e0368032771dfa74d
jayantsaraf <sarafjayant24@gmail.com>
2020-11-01 11:39:59 +0530

    [jayant]Add. added given hotels and details

---
+++ HotelReservationSystem/Hotel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public class Hotel
+    {
+        string mNameOfHotel;
+        int mRegularRate;
+        /// <summary>
+        /// Constructor to assign default values
+        /// </summary>
+        public Hotel()
+        {
+            this.mNameOfHotel = null;
+            this.mRegularRate = 0;
+        }
+        /// <summary>
+        /// Constructor to assign values given by user
+        /// </summary>
+        public Hotel(string name, int regularRate)
+        {
+            this.mNameOfHotel = name;
+            this.mRegularRate = regularRate;
+        }
+    }
+}
